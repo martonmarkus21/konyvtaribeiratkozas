045ed41b7a3534777dd018b69b696123b88fcaa6
Márkus Márton <markus.marton@petrik.hu>
2025-11-25 13:35:46 +0100

    Olvasó osztály létrehozása

---
+++ konyvtaribeiratkozas/Olvaso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvtaribeiratkozas
+{
+	public class Olvaso
+	{
+		public string Nev { get; set; }
+		public int Eletkor { get; set; }
+		public string Mufaj { get; set; }
+		public string Ertesitesek { get; set; }
+		public string Tagsag { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Nev};{Eletkor};{Mufaj};{Ertesitesek};{Tagsag}";
+		}
+	}
+}
