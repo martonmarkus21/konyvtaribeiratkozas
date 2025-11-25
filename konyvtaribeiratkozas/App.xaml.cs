2909d093abf8ed1a42494cdd6af9f9c907b3b3c0
Márkus Márton <markus.marton@petrik.hu>
2025-11-25 13:27:52 +0100

    Add project files.

---
+++ konyvtaribeiratkozas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace konyvtaribeiratkozas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
